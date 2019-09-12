620cb5dabd15207379dba2a913f370afc3b7321f
GabrielWechta <wechtagabriel@gmail.com>
2019-09-12 12:04:12 +0200

    Good commit(12.09.2019) Basic database is working, I like it.

--- RPT_Application/src/main/resources/data.sql
+++ RPT_Application/src/main/resources/data.sql
@@ -1,1 +1,6 @@
 insert into student values(1001,'Geralt','e134')
+insert into student values(1002,'Yennefer','a224')
+insert into student values(1003,'Ciri','c234')
+insert into student values(1004,'Triss','t224')
+insert into student values(2222,'Cirilla','c114')
+
